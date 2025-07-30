dbb629ef71fb8b876d88743087c2e1c9d035a6d6
klzomunuve <klzoalfredoh@gmail.com>
2025-07-30 08:27:03 +0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,17 @@
+function showStudy() {
+  const study = document.getElementById("study");
+  study.innerText = "University of Embu";
+  study.classList.remove("hidden");
+}
+
+function showAge() {
+  const age = document.getElementById("age");
+  age.innerText = "20 years old";
+  age.classList.remove("hidden");
+}
+
+function showContacts() {
+  const contacts = document.getElementById("contacts");
+  contacts.innerText = "WhatsApp: 0759729937";
+  contacts.classList.remove("hidden");
+}
