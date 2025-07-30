6cfcaa3a3168a8e2b200c832da5ed77db80cf6df
klzomunuve <klzoalfredoh@gmail.com>
2025-07-30 09:14:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function showStudy() {
+  const study = document.getElementById("study");
+  study.innerText = "University of Embu";
+  study.classList.remove("hidden");
+}
+
+function showAge() {
+  const age = document.getElementById("age");
+  age.innerText = "20 years old";
+  age.classList.remove("hidden");
+}
+
+function showTribe() {
+  const tribe = document.getElementById("tribe");
+  tribe.innerText = "Kamba";
+  tribe.classList.remove("hidden");
+}
+
+function showHobbies() {
+  const hobbies = document.getElementById("hobbies");
+  hobbies.innerText = "Coding, Reading, Music";
+  hobbies.classList.remove("hidden");
+}
+
+function showSkills() {
+  const skills = document.getElementById("skills");
+  skills.innerText = "HTML, CSS, JavaScript";
+  skills.classList.remove("hidden");
+}
+
+function showContacts() {
+  const contacts = document.getElementById("contacts");
+  contacts.innerText = "WhatsApp: 0759729937";
+  contacts.classList.remove("hidden");
+}
